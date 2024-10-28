7ec5bf03006a2c257ad8d3a84a83c17c3a513e89
Axodaraph <darielnotion@gmail.com>
2024-10-28 06:19:09 -0400

    Plugins

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import MediaPlayer from './MediaPlayerInit.js'
+import MediaPlayer from './MediaPlayerInit.js';
+import AutoPlay from '../plugins/AutoPlay.js';
 const video = document.querySelector("video");
 const button = document.querySelector("button");
-const player = new MediaPlayer({el : video});
+const player = new MediaPlayer({el : video, plugins: [new AutoPlay()]});
+
 button.onclick = () => player.togglePlay(); 
-        /* button.onclick = () => player.media.play(); */
-        /* function handleButtonClick(){
-            if(player.media.paused){
-            button.onclick = () => player.media.play();
-            button.textContent = 'Play';
-            } else {
-            button.onclick = () => player.media.pause();
-            button.textContent = "Pause";
-            }
-        } */
-        /* button.addEventListener("click", handleButtonClick); */
