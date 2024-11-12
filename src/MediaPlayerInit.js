5aa4412ac2ce21577d67ae3c6e4b1822e485ba64
Axodaraph <darielnotion@gmail.com>
2024-11-12 10:37:11 -0500

    change in botton muted

--- src/MediaPlayerInit.js
+++ src/MediaPlayerInit.js
@@ -1,37 +1,46 @@
-class MediaPlayer {
+function MediaPlayer(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+}
+MediaPlayer.prototype._initPlugins = function (){
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        get muted() {
+            return this.media.muted;
+        },
+
+        set muted(value){
+            this.media.muted = value;
+        }
+    };
     
-
-    constructor(config) {
-        this.media = config.el;
-        this.plugins = config.plugins || [];
-
-        this._initPlugins();
-    }
-    _initPlugins() {
-        this.plugins.forEach(plugin => {
-            plugin.run(this);
-        });
-    }
-    play() {
+    this.plugins.forEach(plugin => {
+        plugin.run(player);
+    });
+};
+MediaPlayer.prototype.play = function(){
+    this.media.play();
+};
+MediaPlayer.prototype.pause = function(){
+    this.media.pause();
+};
+MediaPlayer.prototype.togglePlay = function() {
+    if(this.media.paused){
         this.media.play();
-    }
-    pause() {
+    } else {
         this.media.pause();
     }
-    togglePlay() {
-        if (this.media.paused) {
-            this.media.play();
-        } else {
-            this.media.pause();
-        }
-    }
-    mute() {
-        this.media.muted = true;
-    }
-    unmute() {
-        this.media.muted = false;
-    }
-}
+};
+MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+};
+MediaPlayer.prototype.unmute = function(){
+    this.media.muted = false;
+};
 /* MediaPlayer.prototype.changeSound = function() {
     if(this.media.muted){
         this.media.muted = false;
